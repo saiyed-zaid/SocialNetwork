6635b123fad70983ee6769e3b076cb7cb742d260
saiyad zaid <zss@narola.email>
2020-03-05 09:48:22 +0530

    nodeserver setup completed in app.js

--- app.js
+++ app.js
@@ -2,18 +2,21 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
+const getRoutes = require('./routes/get');
 /* Import Required Packages END*/
 
+
 /* Registering middleware BEGIN*/
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 /* Registering middleware END*/
 
 /* Handling Requests BEGIN */
-app.use("/", (req, res, next) => {});
+app.get("/", getRoutes.getPosts);
 /* Handling Requests END */
 
 /* Error Handling Middleware BEGIN */
 app.use((error, req, res, next) => {});
 /* Error Handling Middleware END */
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
