a8c4add5c423563e6df29c890d7095db5c8160c6
saiyad zaid <zss@narola.email>
2020-05-14 17:35:32 +0530

    Added Services

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,186 @@
+import { validateAll } from "indicative/validator";
+import { isAuthenticated } from "../auth/index";
+
+export default class Authservice {
+  async loginUser(data) {
+    const rules = {
+      email: "required|email|string",
+      password: "required|string",
+    };
+
+    const messages = {
+      required: "{{field}} field is required.",
+      "email.email": "Enter a Valid Email.",
+    };
+
+    try {
+      await validateAll(data, rules, messages);
+
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/api/signin`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: data.email,
+              password: data.password,
+            }),
+          }
+        ).then((result) => {
+          return result.json().then((result) => {
+            return result;
+          });
+        });
+
+        if (response.error) {
+          return Promise.reject({
+            responseError: response.error,
+          });
+        } else {
+          return response;
+        }
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    } catch (errors) {
+      var formattedErrors = {};
+      errors.forEach((error) => {
+        formattedErrors[error.field] = error.message;
+      });
+
+      return Promise.reject(formattedErrors);
+    }
+  }
+
+  async registerUser(data) {
+    const rules = {
+      name: "required|alpha",
+      email: "required|email|string",
+      password: "required|string|confirmed|min:5",
+      password_confirmation: "required|string",
+    };
+
+    const messages = {
+      required: "{{field}} Field is Required.",
+      "email.email": "Enter a Valid Email.",
+      "password.confirmed": "Password Does Not Matched.",
+    };
+
+    console.log("bday", new Date(data.year, data.month - 1, data.day));
+
+    try {
+      await validateAll(data, rules, messages);
+
+      var user = {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        dob: new Date(data.year, data.month - 1, data.day + 1),
+      };
+
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/api/signup`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+          }
+        );
+
+        if (response.err) {
+          return Promise.reject(response.err);
+        } else {
+          return await response.json();
+        }
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    } catch (errors) {
+      var formattedErrors = {};
+      errors.forEach((error) => {
+        formattedErrors[error.field] = error.message;
+      });
+
+      return Promise.reject(formattedErrors);
+    }
+  }
+
+  async signout(next) {
+    const token = isAuthenticated().user.token;
+    return fetch(`${process.env.REACT_APP_API_URL}/api/signout`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (typeof window !== "undefined") {
+          localStorage.removeItem("jwt");
+        }
+        next();
+
+        return response.json();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  async changePassword(data, token) {
+    const rules = {
+      oldPassword: "required|string",
+      password: "required|string|confirmed|min:6",
+      password_confirmation: "required|string",
+    };
+
+    const messages = {
+      required: "{{field}} Field is Required.",
+      "password.confirmed": "Password Does Not Matched.",
+    };
+
+    try {
+      await validateAll(data, rules, messages);
+
+      var formData = {
+        oldPassword: data.oldPassword,
+        password: data.password,
+        password_confirmation: data.password_confirmation,
+      };
+
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/api/changePassword`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+        return await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    } catch (errors) {
+      //console.log(data);
+
+      var formattedErrors = {};
+      errors.forEach((error) => {
+        formattedErrors[error.field] = error.message;
+      });
+
+      return Promise.reject(formattedErrors);
+    }
+  }
+}
---
+++ frontend/src/services/user.js
@@ -0,0 +1,225 @@
+import React, { Component } from "react";
+
+export default class user extends Component {
+  /**
+   * Api For Reading Data From Database
+   *
+   * @param {string} userId   User Id Of The Logged In User
+   * @param {string} token    token Of The Logged In User
+   *
+   * @returns {json}
+   */
+  async read(userId, token) {
+    const user = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "Application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return await user.json({ user });
+  }
+
+  /**
+   * Api for Listing All The Users
+   *
+   * @returns {json}
+   */
+  async list() {
+    const users = await fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
+      method: "GET",
+    });
+    return await users.json({ users });
+  }
+
+  /* async list  ()  {
+    const users = await axios.get(`${process.env.REACT_APP_API_URL}/api/users`);
+    return await users.data;
+  };
+   */
+  /**
+   * Api For Deleting The User Profile
+   *
+   * @param {string} userId         User Id Of The Logged In User
+   * @param {string} tokentoken     Of The Logged In User
+   */
+  async remove(userId, token) {
+    const deleteUser = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/${userId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "Application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return await deleteUser.json();
+  }
+
+  /**
+   * Api For Updating The UserData In Database
+   *
+   * @param {string} userId    User Id Of The Logged In User
+   * @param {string} token     token Of The Logged In User
+   * @param {json} user        User data
+   */
+  async update(userId, token, user) {
+    console.log("api", userId, token, user);
+
+    const userData = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/${userId}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: user,
+      }
+    );
+
+    return await userData.json();
+  }
+
+  /**
+   * Api for Updating The User Data In Browser Localstorage
+   *
+   * @param {json} user       user Data
+   * @param {function} next  Function To be Executed After Updating Data
+   */
+  async updateUser(user, next) {
+    if (typeof window != "undefined") {
+      if (JSON.parse(localStorage.getItem("jwt")).user) {
+        let auth = JSON.parse(localStorage.getItem("jwt"));
+        auth.user = { token: auth.user.token, ...user };
+        localStorage.setItem("jwt", JSON.stringify(auth));
+        next();
+      }
+    }
+  }
+
+  /**
+   * Api to Follow Peoples
+   *
+   * @param  {string}  userId
+   * @param  {string}  token
+   * @param  {string}  followId
+   */
+  async follow(userId, token, followId) {
+    const userData = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/follow/${userId}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ userId, followId }),
+      }
+    );
+
+    return await userData.json();
+  }
+
+  /**
+   * Api to unFollow Peoples
+   *
+   * @param  {string}  userId
+   * @param  {string}  token
+   * @param  {string}  followId
+   */
+  async unfollow(userId, token, unfollowId) {
+    const userData = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/unfollow/${userId}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ userId, unfollowId }),
+      }
+    );
+
+    return await userData.json();
+  }
+
+  /**
+   * Api For Fetching New User Except Our Following
+   *
+   * @param {string} userId
+   * @param {string} token
+   */
+  async findPeople(userId, token) {
+    const user = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/findpeople/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return await user.json();
+  }
+
+  /**
+   * Api For Fetching messages for user
+   *
+   * @param {string} userId   User Id Of The Logged In User
+   * @param {string} token    token Of The Logged In User
+   *
+   * @returns {json}
+   */
+  async fetchMessage(senderId, receiverId, token) {
+    const messages = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/messages`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "Application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          sender: senderId,
+          receiver: receiverId,
+        }),
+      }
+    );
+
+    return await messages.json({ messages });
+  }
+
+  async getOnlineUsers(userId, token) {
+    const onlineUsers = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/user/getonline/${userId}`,
+      {
+        method: "get",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "Application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return await onlineUsers.json({ onlineUsers });
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
