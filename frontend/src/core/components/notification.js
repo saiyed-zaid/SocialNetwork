842fe7d39993e011cba1ac7dbe3f0fc030284644
saiyad zaid <zss@narola.email>
2020-05-29 10:12:02 +0530

    no message

---
+++ frontend/src/core/components/notification.js
@@ -0,0 +1,175 @@
+import React from "react";
+import {
+  newFollowersList,
+  /* isFollowStatusChange */ readPost,
+} from "../api/getNotification";
+import { Link } from "react-router-dom";
+
+import Follow from "./getNewFollower";
+import { isAuthenticated } from "../../auth";
+
+class Notification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasNewFollow: false,
+      timer: null,
+      newFollowerList: [],
+      hasNewLikes: false,
+      newLikesList: [],
+      hasNewComment: false,
+      newCommentList: [],
+    };
+  }
+
+  componentDidMount() {
+    //New Follower List
+    newFollowersList()
+      .then((data) => {
+        var newFollowerList = [];
+        if (data.length > 0) {
+          data.forEach((follower, i) => {
+            newFollowerList.push({
+              id: follower.user._id,
+              name: follower.user.name,
+              followedFrom: follower.followedFrom,
+            });
+          });
+
+          if (newFollowerList.length > 0) {
+            this.setState({
+              hasNewFollow: true,
+              newFollowerList: newFollowerList,
+            });
+          }
+
+          if (this.state.hasNewFollow) {
+            //setTimeout(this.followStatusChange, 16000);
+          }
+        }
+      })
+      .catch((err) => {
+        if (err) {
+          console.log("Error while fetching new Followers", err);
+        }
+      });
+
+    try {
+      readPost().then((data) => {
+        let newLikesList = [];
+        let newCommentsList = [];
+        if (data.length > 0) {
+          data.forEach((post) => {
+            //Likes Notification
+            post.likes.forEach((like, i) => {
+              if (like.user._id !== isAuthenticated().user._id) {
+                newLikesList.push({
+                  id: like.user._id,
+                  postId: post._id,
+                  name: like.user.name,
+                  likedFrom: like.likedFrom,
+                });
+              }
+            });
+
+            //Comment Notification
+            post.comments.forEach((comment, i) => {
+              if (comment.postedBy._id !== isAuthenticated().user._id) {
+                newCommentsList.push({
+                  id: comment.postedBy._id,
+                  postId: post._id,
+                  name: comment.postedBy.name,
+                  commentedFrom: comment.created,
+                });
+              }
+            });
+
+            if (newCommentsList.length > 0 || newCommentsList.length > 0) {
+              this.setState({
+                hasNewComment: true,
+                newCommentList: newCommentsList,
+                hasNewLikes: true,
+                newLikesList: newLikesList,
+              });
+            }
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  /* likeStatusChange = () => {
+    if (this.state.hasNewLikes) {
+      const toast = document.querySelectorAll(".noti");
+      toast.forEach((t) => {
+        t.classList.replace("show", "hide");
+      });
+      clearTimeout(this.state.timer);
+      this.setState({ hasNewLikes: false });
+    }
+  }; followStatusChange = () => {
+    if (this.state.hasNewFollow) {
+      isFollowStatusChange();
+      const toast = document.querySelectorAll(".noti");
+      toast.forEach((t) => {
+        t.classList.replace("show", "hide");
+      });
+      clearTimeout(this.state.timer);
+      this.setState({ hasNewFollow: false });
+    }
+  }; */
+
+  render() {
+    return (
+      <li className="nav-item dropdown float-right">
+        <a
+          className="nav-link "
+          href="/"
+          id="navbarDropdownMenuLink"
+          role="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <i className="far fa-bell text-light " />
+          {this.state.newFollowerList.length > 0 ||
+          this.state.newLikesList.length > 0 ||
+          this.state.newCommentList.length > 0 ? (
+            <span className="badge badge-warning ml-1">
+              {this.state.newFollowerList.length +
+                this.state.newLikesList.length +
+                this.state.newCommentList.length}
+            </span>
+          ) : null}
+        </a>
+
+        <div className="dropdown-menu dropdown-menu-lg-right noti-toggle">
+          {(this.state.newFollowerList.length > 0 && (
+            <>
+              <span
+                // to="/"
+                className="dropdown-item "
+                style={{ display: "flex", justifyContent: " center" }}
+              >
+                <span className=" text-muted text-sm">
+                  <Follow
+                    newFollowers={this.state.newFollowerList}
+                    newLikes={this.state.newLikesList}
+                    newComments={this.state.newCommentList}
+                  />
+                </span>
+              </span>
+            </>
+          )) || (
+            <span className="dropdown-item dropdown-header">
+              No Notifications
+            </span>
+          )}
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Notification;
